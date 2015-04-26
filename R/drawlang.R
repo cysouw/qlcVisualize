7970dd45d8288178b9233605fb205e47bd030e5d
Michael Cysouw <cysouw@mac.com>
2015-04-26 20:57:17 +0200

    revise drawlang

--- R/drawlang.R
+++ R/drawlang.R
@@ -1,40 +1,48 @@
-drawLang <- function( name, points, data
-						, col = "black"
+drawLang <- function( points, data
+            , main = NULL
+						, col = NULL
+            , offset = FALSE
 						, min.freq = 5
-						, levels = c(.41,.45,.51)
-						, lambda = NA
+						, levels = c(41,45,51)
+						, lambda = NULL
 						, legend.position = "bottomleft"
-						, font = NULL
 						, cex = 0.7
+						, font = NULL
 						, file.out = NULL
 						, ... ) {
 
 	# preparations
-	if (!is.null(dim(data))) {
-		data <- as.factor(data[,name])
-	} else {
-		data <- as.factor(data)
+  if (is.list(points)) {
+    points <- cbind(points$x, points$y)
+  }
+
+  if (is.null(dim(data))) {
+    data <- as.factor(data)
+    data <- t(sapply(levels(data),function(x){as.numeric(data==x)}))
 	}
 
-	if (is.list(points)) {
-		points <- cbind(points$x, points$y)
-	}
+  if (offset) {
+    off <- mean(diff(sort(points[,1])))/2
+  }
 
-	# which words to include in graphic
-	freq <- sort( table(data), decreasing = TRUE)
-	freq <- freq[freq >= min.freq]
-	words <- names(freq)
+  # which words to include in graphic
+  freq <- apply(data,1,function(x){sum(na.omit(x != 0))})
+  freq[freq <= min.freq] <- NA
+  data <- data[order(freq, decreasing = TRUE, na.last = NA), ]
 
-	# ignored because of low frequency
-	ignore <- unlist(sapply(words, function(x) {which(data == x)}))
-
-	# zero coordinates to get nicer round areas
-	zeros <- addZero(points, show = FALSE, ...)
+  # points ignored because of low frequency words or NAs
+  stats <- data
+  stats[is.na(stats)] <- 0
+  ignore <- which(colSums(stats) == 0)
 
 	# repeat colors when necessary
-	col <- rep(col, times = ceiling(length(words)/length(col)))
+  if (is.null(col)) { col <- "black" }
+	col <- rep(col, times = ceiling(nrow(data)/length(col)))
 
-	# open plotting device
+  # zero coordinates to get nicer plotted areas
+  zeros <- addZero(points, show = FALSE, ...)
+
+	# open plotting device for saving file
 	if (!is.null(file.out)) {
 		quartz(	width = 6
 				, height = 6
@@ -45,41 +53,38 @@ drawLang <- function( name, points, data
 
 	# prepare plotting frame
 	plot( points
-		, type = "n"
-		, xlab = ""
-		, ylab = ""
-		, main = name
-		, xaxt = "n"
-		, yaxt = "n"
-		)
+    		, type = "n"
+    		, xlab = ""
+    		, ylab = ""
+    		, main = main
+    		, xaxt = "n"
+    		, yaxt = "n"
+    		)
 
-	# add large grey crosses for non-included data (individual cases)
-	points( points[-ignore,]
-			, col = "grey"
-			)
-
-	# add small grey crosses for missing data
-	points( points[is.na(data),]
-			, col = "grey"
-			, cex = .5
-			, pch = 4
-			)
+	# add large grey crosses for non-included data (low frequency)
+	points( points[ignore,]
+    			, col = "grey"
+          , cex = .5
+    			)
 
 	# loop through words and make contours
-	for (i in 1:length(words)) {
+	for (i in 1:nrow(data)) {
 
 		# determine height
-		h <- as.numeric(data == words[i])
+		h <- data[i,]
 		h0 <- rep.int(0, times = length(zeros$x))
 
 		# make countours
-		kriging <- Krig(  x = cbind(c(points[,1], zeros$x), c(points[,2], zeros$y))
+    if (is.null(lambda)) { lambda  <- NA }
+
+		kriging <- Krig(x = cbind(c(points[,1], zeros$x),
+                              c(points[,2], zeros$y))
 						, Y = c(h, h0)
 						, lambda = lambda
 						)
 		surface <- predictSurface(kriging)
 		contour(surface
-				, levels = levels
+				, levels = max(stats)*levels/100
 				, lwd = seq(0.5, 2, length.out = length(levels))
 				, col = col[i]
 				, drawlabels = FALSE
@@ -87,7 +92,14 @@ drawLang <- function( name, points, data
 				)
 
 		# add symbols
-		points( points[data == words[i],]
+    if (offset) {
+      angle <- 2*pi*i/nrow(data)
+      rotation <- c(sin(angle),cos(angle)) * off
+    } else {
+      rotation <- c(0,0)
+    }
+
+		points( points[data[i,] > 0, ] + rotation
 				, pch = i-1
 				, cex = 0.7
 				, col = col[i]
@@ -98,15 +110,15 @@ drawLang <- function( name, points, data
 	if (!is.null(legend)) {
 		par(family = font)
 		legend( legend.position
-				, pch = 1:length(words)-1
-				, legend = words
-				, cex = cex
-				, col = col[1:length(words)]
-				)
+    				, pch = 1:nrow(data)-1
+    				, legend = rownames(data)
+    				, cex = cex
+    				, col = col[1:nrow(data)]
+    				)
 		par(family = NULL)
 	}
 
-	# close plotting device
+	# close plotting device when saving to file
 	if (!is.null(file.out)) {
 		dev.off()
 		}
